6e307c19c608d9d8590844e75a0fd24c226dfd09
OJ Adeyemi <ojieadeyemi@gmail.com>
2025-05-28 23:08:25 -0400

    minor update to dependencies

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://user:password@localhost/usports")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://user:password@localhost:5432/usports")
